0e058a74d70c31faef24369bc6beca0b2ba4830f
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-12-07 01:03:07 +0200

    added producerConsumer

---
+++ ProducerConsumerMain.java
@@ -0,0 +1,37 @@
+public class ProducerConsumerMain{
+    public static void main(String[] args) {
+        ProducerConsumer pc=new ProducerConsumer(9);
+        Thread t1=new Thread(()->{
+            for(int i=0; i <10; i++){
+                try{
+                    pc.produce(i);
+                    //Thread.sleep(10);
+                }
+                catch(InterruptedException e)
+        {
+            e.printStackTrace();
+                
+                }
+            }
+
+        });
+
+         Thread t2=new Thread(()->{
+            for(int i=0; i <10; i++){
+                try{
+                    pc.consume();
+                    //Thread.sleep(100);
+
+                }
+                catch(InterruptedException e)
+        {
+            e.printStackTrace();
+                
+                }
+            }
+
+        });
+        t1.start();
+        t2.start();
+    }
+}
